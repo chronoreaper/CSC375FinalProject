7caf18207ead702500e6a2e15c8558c11d2f2013
chronoreaper <chris.dd.29@gmail.com>
2022-04-22 15:48:24 -0400

    Made dqn a little nicer

--- dqn.py
+++ dqn.py
@@ -1,18 +1,36 @@
 import gym
 from env import ClawEnv
+from absl import app
+import matplotlib.pyplot as plt
 from stable_baselines3 import DQN
-from stable_baselines3.common.env_checker import check_env
 
 env = ClawEnv(renders=True, isDiscrete=True, removeHeightHack=False, maxSteps=20)
+def plot_rewards(running_rewards):
+    plt.plot(running_rewards)
+    plt.draw()
+    plt.pause(0.01)
+    pass
 
-model = DQN("MlpPolicy", env, verbose=1, buffer_size=10000)
-model.action_space = env.action_space
-model.learn(total_timesteps=1000, log_interval=4)
+def main(argv):
 
-obs = env.reset()
-while True:
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
-    env.render()
-    if done:
-      obs = env.reset()
+  model = DQN("MlpPolicy", env, verbose=1, buffer_size=10000)
+  model.action_space = env.action_space
+  model.learn(total_timesteps=1000, log_interval=4)
+
+  obs = env.reset()
+
+  running_rewards = []
+  
+  while True:
+      action, _states = model.predict(obs, deterministic=True)
+      obs, reward, done, info = env.step(action)
+      env.render()
+      if done:
+        obs = env.reset()
+      
+      running_rewards.append(reward)
+
+      plot_rewards(running_rewards)
+
+if __name__ == '__main__':
+  app.run(main)
